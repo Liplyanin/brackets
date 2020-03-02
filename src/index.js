f05a092330a3fdb30bb291d8fe8fa4a7503dc95b
Maxim Liplyanin <liplyanin@yandex.ru>
2020-03-02 23:28:59 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+ for (let i = 0; i < bracketsConfig.length;) {
+   let strOfArr = bracketsConfig[i].join("");
+   if (str.includes(strOfArr)) {
+        str=str.replace(strOfArr, "");
+        i=0;
+      } else i++;
+    
+  }
+  return str.length==0 ? true : false;
+  
 }
